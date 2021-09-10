cf7dc07e7d234ca403be695a6caf434b423ea814
IT19952062 <88031917+IT19952062@users.noreply.github.com>
2021-09-10 15:56:23 +0530

    Update AddHealthServlet.java

--- Brave_Blood_Donation_System/src/com/braveBloodDonation/servlets/AddHealthServlet.java
+++ Brave_Blood_Donation_System/src/com/braveBloodDonation/servlets/AddHealthServlet.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
+import javax.servlet.ServletException; 
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
