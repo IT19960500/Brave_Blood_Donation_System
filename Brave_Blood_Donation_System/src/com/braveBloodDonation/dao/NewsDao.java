57cd4ea09a3693062c2b25bf6b4018f6c872050f
IT19952062 <88031917+IT19952062@users.noreply.github.com>
2021-09-10 15:51:20 +0530

    Update NewsDao.java

--- Brave_Blood_Donation_System/src/com/braveBloodDonation/dao/NewsDao.java
+++ Brave_Blood_Donation_System/src/com/braveBloodDonation/dao/NewsDao.java
@@ -4,7 +4,7 @@ package com.braveBloodDonation.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.SQLException; 
 import java.util.ArrayList;
 
 
