213098b210d88c492f1b5270dacd035e54b5e594
sandakelum00 <randunusandakelum@gmail.com>
2021-09-28 13:03:48 +0530

    appointment dao

--- Brave_Blood_Donation_System/src/com/braveBloodDonation/dao/AppointmentFormDao.java
+++ Brave_Blood_Donation_System/src/com/braveBloodDonation/dao/AppointmentFormDao.java
@@ -1,62 +1,49 @@
 package com.braveBloodDonation.dao;
 import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import com.braveBloodDonation.entities.Appointment;
+import com.braveBloodDonation.helper.connectionProvider;
 
 public class AppointmentFormDao {
 	
-	private String dbUrl = "jdbc:mysql://localhost:3306/brave_blood_donation_db?autoReconnect=true&useSSL=false";
-	private String dbUname = "root";
-	private String dbPassword = "so1487906*";
-	private String dbDriver = "com.mysql.jdbc.Driver";
+public static boolean insertAppointment (String nic,String name,String bloodgrp, String date,String status) {
+		
+		boolean isSuccess=false;
 	
-	public void loadDriver(String dbDriver)
-	{
 		try {
-			Class.forName(dbDriver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public Connection getConnection()
-	{
-		Connection con = null;
-		try {
-			con = DriverManager.getConnection(dbUrl, dbUname, dbPassword);
+
+			//get our dbconnection
+			Connection con = connectionProvider.getConnection();
+			
+			//insert sql query
+			String sql = "INSERT INTO appointments(nic,name,bloodgrp,date,status) VALUES(?,?,?,?,?)";
+						
+			//create statement
+			java.sql.PreparedStatement stmt = con.prepareStatement(sql);
+			
+			//assign the value to the sql query
+			stmt.setString(1, nic);
+			stmt.setString(2, name);
+			stmt.setString(3, bloodgrp);
+			stmt.setString(4, date);
+			stmt.setString(5, status);
+			//execute query
+			int rs = stmt.executeUpdate();
+			
+			//check insert success or not
+			if(rs>0) {
+				
+				isSuccess=true;
+				
+			}else {
+				isSuccess=false;
+			}	
+		
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			
 		}
-		return con;
-	}
-	
-	
-	public String insert(Appointment appointment)
-	{
-		loadDriver(dbDriver);
-		Connection con = getConnection();
-		String result = "Data entered successfully";
-		String sql = "insert into appointments values(?,?,?,?)";
-		
-		
-		try {
-			PreparedStatement ps = con.prepareStatement(sql);
-		ps.setString(1, appointment.getNIC());
-		ps.setString(2, appointment.getName());
-		ps.setString(3, appointment.getBloodgrp());
-		ps.setString(4, appointment.getDate());
-		ps.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			result = "Data not entered";
-		}
-		return result;
+		return isSuccess;
 	}
 }
